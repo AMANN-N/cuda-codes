0991de06cf37bfa24ab81c5d3caf2467ad5f740c
amansingh <aman.singh@ofbusiness.in>
2025-03-29 18:27:52 +0530

    chanes

---
+++ Day4/mat_transpose.cu
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+#define N 1000
+
+__global__ 
+void mat_transpose(float *A, float *C, int width)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;  // Column 
+    int j = threadIdx.y + blockIdx.y * blockDim.y;  // Row 
+
+    if(i < width && j < width)
+    {
+        C[i * width + j] = A[j * width + i];  // **Corrected swapping**
+    }
+}
+
+int main() {
+    float *h_A, *h_C;
+    float *d_A, *d_C;    
+    size_t size = N * N * sizeof(float);
+
+    h_A = (float*)malloc(size);
+    h_C = (float*)malloc(size);
+
+
+    for (int i = 0; i < N; i++) 
+    {
+        for(int j = 0; j < N; j++)
+        {
+            h_A[i * N + j] = (float)(i + j); 
+            h_C[i * N + j] = 0.0f; 
+        }
+    }
+
+    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_C, size);
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+
+    dim3 block(16, 16);
+    dim3 grid((N + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+
+
+    mat_transpose<<<grid, block>>>(d_A, d_C, N);
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    printf("Transposed Matrix (First 5x5 pixels):\n");
+    for(int i = 0; i < 5; i++)      
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            printf("%0.2f ", h_C[i * N + j]);
+        }
+        printf("\n");
+    }
+
+    cudaFree(d_A);
+    cudaFree(d_C);
+    free(h_A);
+    free(h_C);  
+
+    return 0;
+}
--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 DAY 1 - Setup cuda, Read 1 Chapter of PMPP, Implemented Basic Addition kernel.
 DAY 2 - Coded Matrix Addition Kernel, Read Chapter 2 of PMPP.
 Day 3 - Coded BlurImage and Matrix Multiplication Kernel, Read Chapter 3 of PMPP.
+Day 4 - Transpose Matrix Kernel
