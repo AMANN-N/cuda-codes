d72fbadbce06dac182e3508dd25f856a555bf1b1
amansingh <aman.singh@ofbusiness.in>
2025-03-30 17:42:34 +0530

    Day 5

---
+++ Day5/sum_of_vec_elems.cu
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+#define n 1000
+
+__global__ void calcsum(float *x, float *out, int n) {
+    extern __shared__ float sdata[];
+    int a = threadIdx.x;
+    int b = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    sdata[a] = (b < n) ? x[b] : 0.0f;
+    __syncthreads();
+    
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (a < s) sdata[a] += sdata[a + s];
+        __syncthreads();
+    }
+    
+    if (a == 0) out[blockIdx.x] = sdata[0];
+}
+
+int main() 
+{
+    size_t size = n * sizeof(float);
+    float *h_A = (float*)malloc(size);
+    float *h_output = (float*)malloc(sizeof(float));
+
+    for (int i = 0; i < n; i++) h_A[i] = 1.0f;
+    
+    float *d_A, *d_output;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_output, ((n + 255) / 256) * sizeof(float));
+    
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    
+    int threads = 256;
+    int blocks = (n + threads - 1) / threads;
+
+
+    calcsum<<<blocks, threads, threads * sizeof(float)>>>(d_A, d_output, n);
+
+
+    cudaDeviceSynchronize();
+    
+    float *h_partialSums = (float*)malloc(blocks * sizeof(float));
+    cudaMemcpy(h_partialSums, d_output, blocks * sizeof(float), cudaMemcpyDeviceToHost);
+    
+    float sum = 0.0f;
+    for (int i = 0; i < blocks; i++) sum += h_partialSums[i];
+    
+    printf("CUDA Parallel Sum = %.1f (expected 1000.0)\n", sum);
+    
+    cudaFree(d_A); cudaFree(d_output);
+    free(h_A); free(h_output); free(h_partialSums);
+    return 0;
+}
--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@ DAY 1 - Setup cuda, Read 1 Chapter of PMPP, Implemented Basic Addition kernel.
 DAY 2 - Coded Matrix Addition Kernel, Read Chapter 2 of PMPP.
 Day 3 - Coded BlurImage and Matrix Multiplication Kernel, Read Chapter 3 of PMPP.
 Day 4 - Transpose Matrix Kernel
+Day 5 - Sum of all elements of an array.
